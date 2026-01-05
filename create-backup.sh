41283141b967e1b965be14b1ac458c9a5e217fa2
tls1235 <yotam.mini3@gmail.com>
2026-01-05 17:49:17 +0200

    1

--- create-backup.sh
+++ create-backup.sh
@@ -10,13 +10,13 @@ cp ~/.ssh backup/.ssh
 # from here on this is creating the file to update the system
 echo "sudo pacman -Syu --needed reflector
 sudo reflector --latest 30 --protocol https --fastest 5 --sort rate --age 12 --save /etc/pacman.d/mirrorlist
-cp backups/pacman.conf /etc/pacman.conf
+cp pacman.conf /etc/pacman.conf
 sudo pacman -Syyu --needed $(pacman -Qq | tr '\n' ' ')
-cp backup/.config ~/.config
-cp backup/.zshrc ~/.zshrc
-cp backup/.bashrc ~/.bashrc
-cp -r backup/important ~/important
-cp backup/.gitconfig ~/.gitconfig
-cp backup/.gitignore ~/.gitignore
-cp backup/.ssh ~/.ssh
+cp .config ~/.config
+cp .zshrc ~/.zshrc
+cp .bashrc ~/.bashrc
+cp -r important ~/important
+cp .gitconfig ~/.gitconfig
+cp .gitignore ~/.gitignore
+cp .ssh ~/.ssh
 chsh -s $(echo $SHELL)" > backup/zupdate-system.sh
